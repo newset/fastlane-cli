ee380736f66f097d906ba0f13439453c106d5c37
insane <luojieyy@gmail.com>
2020-09-08 14:37:49 +0800

    feat: boot 命令完成

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ fl create [name] --type=0 --apiPrefix=/sso-auth --dist=/rapp/sso-auth
 | 0 \| hybrid | hybrid | [mobile-native](https://git.doctorwork.com/qiexr/public-group/templates/mobile-native) | 多端通用       |
 | 1 \| admin  | admin  | [umi-dash](https://git.doctorwork.com/qiexr/public-group/templates/umi-dash)           | 专注管理后模板 |
 | 2 \| node   | node   | [nodejs](https://git.doctorwork.com/qiexr/public-group/templates/nodejs)               | Node 端模板    |
-| cli         | cli    | [nodejs](https://git.doctorwork.com/qiexr/public-group/templates/cli)                  | 命令行 模板    |
+| cli         | cli    | [cli](https://git.doctorwork.com/qiexr/public-group/templates/cli)                     | 命令行 模板    |
 
 > dist
 
--- src/command/boot.ts
+++ src/command/boot.ts
@@ -2,7 +2,7 @@
  * fl boot mac
  */
 
-import { Argv } from "yargs";
+import yargs, { Argv } from "yargs";
 const ora = require("ora");
 const promisify = require("util").promisify;
 const path = require("path");
@@ -14,8 +14,8 @@ const { spawn } = require("../utils/shell");
 export const command = "boot [os]";
 export const desc = "初始化 新电脑";
 
-export const builder = (yargs: Argv) => {
-  return yargs
+export const builder = (yargs: Argv) =>
+  yargs
     .positional("os", {
       description: "操作系统",
       default: "mac",
@@ -25,35 +25,68 @@ export const builder = (yargs: Argv) => {
         alias: "i",
         desciption: "是否安装iterm2",
       },
+      brew: {
+        type: "boolean",
+        alias: "b",
+        desciption: "是否强制重新安装Homebrew",
+      },
     });
-};
 
-export const brew = async () => {
-  const tsinghua = (git: TemplateStringsArray) =>
-    `https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/${git}`;
-  const core = "homebrew-core.git";
+export const brew = async (force = false) => {
   // 安装 Homebrew, 先检查brew后再执行
   try {
-    execSync("which brews");
+    execSync("which brew");
+
+    if (!force) return;
+    ora().start().info("重装Homebrew...");
   } catch (err) {
-    await spawn("./shell/brew.sh");
+    ora().start().info("安装Homebrew...");
   }
+  await spawn("./shell/brew.sh");
 };
 
-export const zshrc = function () {
+export const zsh = async function () {
+  const fs = require("fs-extra");
+  const home = process.env.HOME;
+  // 检查 目录
+  if (!fs.existsSync(path.join(home, ".oh-my-zsh"))) {
+    // 安装 oh-my-zsh
+    await spawn("./shell/zsh.sh");
+  }
   // 编辑 .zshrc
-  const plugins =
-    "git z pkg sublime zsh-autosuggestions vscode zsh_reload colored-man-pages zsh-syntax-highlighting sudo";
-
+  const plugins = "git z sublime zsh-autosuggestions vscode zsh_reload colored-man-pages zsh-syntax-highlighting sudo".split(
+    " "
+  );
+  const zshrc = path.join(home, ".zshrc");
   // 读取 .zshrc
+  const plugin_reg = /\nplugins=\(([^)]+)/;
+  const rc = fs.readFileSync(zshrc, { encoding: "utf-8" });
+  const [, enabled] = plugin_reg.exec(rc);
+  const set = new Set([
+    ...plugins,
+    ...enabled
+      .replace(/[\r\n\s]+/g, " ")
+      .split(" ")
+      .filter(Boolean),
+  ]);
+  const merged = Array.from(set);
+  const modified = rc.replace(
+    plugin_reg,
+    "\nplugins=(" + merged.join("\n\t") + "\n"
+  );
+  fs.writeFile(zshrc, Buffer.from(modified));
 };
 
-export const handler = async (args: Argv) => {
-  ora().start().info("开始准备基础开发工具...");
-  // 安装 oh-my-zsh
-  await spawn("./shell/zsh.sh");
+type CommandArg = {
+  brew: boolean;
+};
 
-  await brew();
+export const handler = async (args: CommandArg) => {
+  ora().start().info("开始准备基础开发工具...");
+
+  await zsh();
+
+  await brew(args.brew);
 
   ora().start().info("更新 brew 仓库");
   await spawn("./shell/repo.sh");
